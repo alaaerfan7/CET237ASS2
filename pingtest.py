160111aaf79af7ce6e093447aac6bf7ff2cc0959
alaaerfan7 <101526295+alaaerfan7@users.noreply.github.com>
2022-05-09 19:21:12 +0300

    Add files via upload

--- pingtest.py
+++ pingtest.py
@@ -1,66 +1,49 @@
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 
-def ping(host):
+
+def ping(host,host1,host2):
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
     """
-
     # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
 
     # Building the command. Ex: "ping -c 1 google.com"
     command = ['ping', param, '1', host]
-   
-    
-    if subprocess.call(command) == 0:
+    command2 = ['ping', param, '2', host1]
+    command3 = ['ping', param, '3', host2]
+
+  #call(...): Runs a command, waits for it to complete, then returns the return code.
+    if subprocess.call(command) == 0:   #if the command provided above is equal to 0, show the msg succeccfully pinged, else ping failed)
+
         msg = 'Ping  ' + host +  ' successfully :)'
+      
     else:
         msg= 'Ping  ' + host +  ' failed :('
-
-    return msg
-host = '172.28.0.3'
-print('\n',ping(host),'\n')
-
-def ping(host1):
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
- # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host1]
-   
     
-    if subprocess.call(command) == 0:
 
-       msg = 'Ping  ' + host1 +  ' successfully :)'
+
+    if subprocess.call(command2) == 0:
+        msg = 'Ping  ' + host1 +  ' successfully :)'
     else:
-
-     msg= 'Ping  ' + host1 +  ' failed :('
-  
-  
-    return msg
-host1 = '172.29.0.2'
-print('\n',ping(host1),'\n')
+        msg= 'Ping  ' + host1 +  ' failed :('
 
 
-def ping(host2):
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
- # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host2]
-   
-    
-    if subprocess.call(command) == 0:
-
-       msg = 'Ping  ' + host2 +  ' successfully :)'
+    if subprocess.call(command3) == 0:
+        msg = 'Ping  ' + host2 +  ' successfully :)'
     else:
+        msg= 'Ping  ' + host2 +  ' failed :('
 
-     msg= 'Ping  ' + host2+  ' failed :('
-  
-  
+
+
     return msg
-host2 = '172.29.0.3'
-print('\n',ping(host2),'\n')
+host = '172.24.0.3'
+host1 = '172.25.0.2'
+host2 = '172.25.0.3'
+print('\n',ping(host,host1,host2),'\n') 
+
 
 
 
